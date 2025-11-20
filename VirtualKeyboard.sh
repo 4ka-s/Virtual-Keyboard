9c60f721a8baf7b9d242edb7da59bdbb2bc7adef
Adrish Das <adrishdas1@hotmail.com>
2025-11-20 19:32:36 +0530

    Fixed onscreen keyboard spawning on main screen only

--- VirtualKeyboard.sh
+++ VirtualKeyboard.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-sleep 1
-quickshell -p ./Notification.qml
+
+sleep 2
+
+monitors=$(hyprctl monitors -j | jq -r '.[].name')
+
+for mon in $monitors; do
+    hyprctl dispatch focusmonitor "$mon"
+    quickshell -p ./Notification.qml &
+    sleep 1
+done
